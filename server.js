33b80a3cf8db4c7faf2afc0dd71137c3643b56bb
kecheste <aben.ezer443@gmail.com>
2023-06-16 01:14:34 -0700

    f

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const port = 9999
+const mongoose = require('mongoose')
+const User = require('./model/user')
+
+mongoose.connect('mongodb://localhost:27017/login-app-db', {
+    useNewUrlParser: true
+})
 
 const app = express()
 app.use('/',express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
 app.post('/api/register', async (req, res) => {
-    console.log(req.body);
     res.json({status: 'ok'})
 })
 
-app.listen(port, () => {
+app.listen(port, ()  => {
     console.log(`server up at ${port}`);
 })
