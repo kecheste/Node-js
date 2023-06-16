0e93324f2a00a35047e054b6ae6c6cae1cdf149f
kecheste <aben.ezer443@gmail.com>
2023-06-16 01:26:55 -0700

    'f'

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const mongoose = require('mongoose')
 const User = require('./model/user')
 
 mongoose.connect('mongodb://localhost:27017/login-app-db', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+
 })
 
 const app = express()
-app.use('/',express.static(path.join(__dirname, 'static')))
+app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
 app.post('/api/register', async (req, res) => {
+    console.log(req.body)
+
     res.json({status: 'ok'})
 })
 
